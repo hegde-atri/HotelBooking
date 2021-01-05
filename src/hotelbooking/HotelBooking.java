e5e617028778fc40e973a1230caf2b2de5561e40
Atri Hegde <iamatrihegde@gmail.com>
2021-01-05 21:58:31 +0000

    initial commit

---
+++ src/hotelbooking/HotelBooking.java
@@ -0,0 +1,28 @@
+package hotelbooking;
+
+
+public class HotelBooking {
+    
+    
+    
+    
+    /*
+    A hotel needs a new toom booking system for their rooms.
+    single - 50
+    double - 75
+    family (four) - 105
+    
+    self catering - free
+    half board - 10
+    full board - 20
+    Hotel offers a discount of 20 percent off of the daily rate for each day that you stay for one week
+    Make a hotel booking system which displays totals before and after discounts.
+    */
+
+    
+    public static void main(String[] args) {
+        
+        
+    }
+    
+}
